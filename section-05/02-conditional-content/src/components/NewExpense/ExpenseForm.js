e28c935ce39915974f38696fa26277b47be64907
JiyeonMin <ichbinmin2@gmail.com>
2022-03-08 16:45:18 +0900

    📝 section-05 : Practice | Conditional Content(solution)

--- section-05/02-conditional-content/src/components/NewExpense/ExpenseForm.js
+++ section-05/02-conditional-content/src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
-  const [show, setShow] = useState(false);
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -42,57 +41,47 @@ const ExpenseForm = (props) => {
     setEnteredDate("");
   };
 
-  const onClickShowInput = () => {
-    setShow(!show);
-  };
   return (
     <form onSubmit={submitHandler}>
-      {show && (
-        <>
-          <div className="new-expense__controls">
-            <div className="new-expense__control">
-              <label>Title</label>
-              <input
-                type="text"
-                onChange={titleChangeHandler}
-                value={enteredTitle}
-              />
-            </div>
-
-            <div className="new-expense__control">
-              <label>Amount</label>
-              <input
-                type="number"
-                min="0.01"
-                step="0.01"
-                onChange={amountChangeHandler}
-                value={enteredAmount}
-              />
-            </div>
-
-            <div className="new-expense__control">
-              <label>Date</label>
-              <input
-                type="date"
-                min="2022-01-01"
-                max="2024-12-31"
-                onChange={dateChangeHandler}
-                value={enteredDate}
-              />
-            </div>
-          </div>
-
-          <div className="new-expense__actions">
-            <button onClick={onClickShowInput}>Cancel</button>
-            <button type="submit">Add Expense</button>
-          </div>
-        </>
-      )}
-      {!show && (
-        <div className="new-expense__actions display">
-          <button onClick={onClickShowInput}>Add New Expense</button>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={enteredTitle}
+          />
         </div>
-      )}
+
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+            value={enteredAmount}
+          />
+        </div>
+
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2022-01-01"
+            max="2024-12-31"
+            onChange={dateChangeHandler}
+            value={enteredDate}
+          />
+        </div>
+      </div>
+
+      <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit">Add Expense</button>
+      </div>
     </form>
   );
 };
