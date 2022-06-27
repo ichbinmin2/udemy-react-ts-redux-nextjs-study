85e2d6f4507f48aa093ae2964a9a6ae1e11c4ea6
JiyeonMin <ichbinmin2@gmail.com>
2022-06-27 14:56:50 +0900

    📝 section-17 : Fetching Meals via Http

--- section-17/01-starting-project/src/components/Meals/AvailableMeals.js
+++ section-17/01-starting-project/src/components/Meals/AvailableMeals.js
@@ -3,43 +3,70 @@ import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 import { useEffect, useState } from "react";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: "m1",
+//     name: "Sushi",
+//     description: "Finest fish and veggies",
+//     price: 22.99,
+//   },
+//   {
+//     id: "m2",
+//     name: "Schnitzel",
+//     description: "A german specialty!",
+//     price: 16.5,
+//   },
+//   {
+//     id: "m3",
+//     name: "Barbecue Burger",
+//     description: "American, raw, meaty",
+//     price: 12.99,
+//   },
+//   {
+//     id: "m4",
+//     name: "Green Bowl",
+//     description: "Healthy...and green...",
+//     price: 18.99,
+//   },
+// ];
 
 const AvailableMeals = () => {
-  const [data, setData] = useState();
+  const [meal, setMeal] = useState([]);
 
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  useEffect(() => {
+    const fetchMaels = async () => {
+      const response = await fetch(
+        "https://react-http2-e19ad-default-rtdb.firebaseio.com/meals.json"
+      ).then();
+
+      const responseData = await response.json();
+
+      // 객체로 받아오기 때문에 배열화 시켜야 함.
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      // console.log(loadedMeals);
+      setMeal(loadedMeals);
+    };
+
+    fetchMaels();
+  }, []);
+
+  console.log("meal", meal);
+  const mealsList = meal.map((item) => (
     <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
+      key={item.id}
+      id={item.id}
+      name={item.name}
+      description={item.description}
+      price={item.price}
     />
   ));
 
--- section-17/section17.md
+++ section-17/section17.md
@@ -3,6 +3,7 @@
 ## 목차
 
 - [Moving "Meals" Data To The Backend](#Meals-데이터를-백엔드로-이동하기)
+- [Fetching Meals via Http](#Http를-통해-Meal-데이터-가져오기)
 
 </br>
 
@@ -13,5 +14,9 @@
 <img width="438" alt="image" src="https://user-images.githubusercontent.com/53133662/175865538-85636935-423b-4315-ba02-3039ba8c43a5.png">
 
 - `AvailableMeals.js` 파일 내부에 있던 더미 데이터 `DUMMY_MEALS` 를 Firebase Realtime Database 에 수기로 작성하여 옮긴다.
+
+</br>
+
+## Http를 통해 Meal 데이터 가져오기
 
 </br>
